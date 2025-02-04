13273abd078f764e57fed5b43b5d58e3a1fde887
AJTECH <145960311+AJTECH0001@users.noreply.github.com>
2025-02-04 17:19:20 +0100

    proofmint

---
+++ Backend/backend.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const backend = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default backend
---
+++ Contract/contract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.6.19;
+
+contract ProofMint {
+    constructor() {
+        
+    }
+}
